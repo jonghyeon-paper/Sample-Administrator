78bbe38c16b1b39681c84930dabe8cea2578363c
jonghyeon <jonghyeon@partner.sk.com>
2017-08-02 17:51:38 +0900

    테스트 파일

--- src/test/java/service/UserTest.java
+++ src/test/java/service/UserTest.java
@@ -39,6 +39,8 @@ public class UserTest extends AbstractJUnit {
 		user.setUserId(USER_ID);
 		user.setUserName("gildong");
 		user.setDepartment("ABS");
+		user.setPosition("123");
+		user.setContact("1111111111111");
 		user.setUseState(UseState.USE);
 		userService.add(user);
 		
