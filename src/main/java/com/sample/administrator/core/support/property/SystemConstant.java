68659bcc7e07ffe1406184e2b2dc6ab0716e87d9
jonghyeon <jonghyeon@partner.sk.com>
2017-08-29 15:04:53 +0900

    -파일 제거

--- src/main/java/com/sample/administrator/core/support/property/SystemConstant.java
+++
@@ -1,8 +0,0 @@
-package com.sample.administrator.core.support.property;
-
-public class SystemConstant {
-	public static final String LOG4J_LOCATION = "log4j.location";
-	public static final String FILE_UPLOAD_DIR = "file.upload.dir";
-	public static final String JSON_VIEW = "jsonView";
-
-}
